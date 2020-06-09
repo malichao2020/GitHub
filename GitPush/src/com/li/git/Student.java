8c558a318114c3c6f5e73bd3e4e8e25b256e4391
malichao2020 <1260584298@qq.com>
2020-06-09 17:15:25 +0800

    将工程推送到远程仓库

---
+++ GitPush/src/com/li/git/Student.java
@@ -0,0 +1,18 @@
+package com.li.git;
+
+import java.util.List;
+
+public class Student {
+	public void addUser(int id,String Username,String password) {
+		//add...
+	}
+    public void deleteUser(int id) {
+    	//delete....
+    }
+    public void updateUser(int id,String newUsername,String newpassword) {
+    	//...
+    }
+    public List<Object> scanfUser(int pageIndex,int pageSize){
+    	return null;
+    }
+}
